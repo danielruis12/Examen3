378e5d4889fb246fe819e1f65977ae4aa78e779c
Daniel Ruiz <daniru2770@gmail.com>
2025-04-23 16:04:04 -0500

    Correcciones hechas en la clase Login y controlador de Login.

--- Clases/clsLogin.cs
+++ Clases/clsLogin.cs
@@ -1,100 +1,7 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using Examen3.Models;
-//using System.Web;
-
-//namespace Examen3.Clases
-//{
-//    public class clsLogin
-//    {
-//        public clsLogin()
-//        {
-//            loginRespuesta = new LoginRespuesta();
-//        }
-//        public DBExamen3Entities dbExamen3 = new DBExamen3Entities();
-//        public Login login { get; set; }
-//        public LoginRespuesta loginRespuesta { get; set; }
-//        public bool ValidarUsuario()
-//        {
-//            try
-//            {
-//                clsCypher cifrar = new clsCypher();
-//                Usuario usuario = dbExamen3.AdministradorITMs.FirstOrDefault(a => a.Usuario == login.Usuario);
-//                if (usuario == null)
-//                {
-//                    loginRespuesta.Autenticado = false;
-//                    loginRespuesta.Mensaje = "Usuario no existe";
-//                    return false;
-//                }
-//                byte[] arrBytesSalt = Convert.FromBase64String(usuario.Salt);
-//                string ClaveCifrada = cifrar.HashPassword(login.Clave, arrBytesSalt);
-//                login.Clave = ClaveCifrada;
-//                return true;
-//            }
-//            catch (Exception ex)
-//            {
-//                loginRespuesta.Autenticado = false;
-//                loginRespuesta.Mensaje = ex.Message;
-//                return false;
-//            }
-//        }
-//        private bool ValidarClave()
-//        {
-//            try
-//            {
-//                Usuario usuario = dbExamen3.AdministradorITMs.FirstOrDefault(a => a.Usuario == login.Usuario && a.Clave == login.Clave);
-//                if (usuario == null)
-//                {
-//                    loginRespuesta.Autenticado = false;
-//                    loginRespuesta.Mensaje = "La clave no coincide";
-//                    return false;
-//                }
-//                return true;
-//            }
-//            catch (Exception ex)
-//            {
-//                loginRespuesta.Autenticado = false;
-//                loginRespuesta.Mensaje = ex.Message;
-//                return false;
-//            }
-//        }
-//        public IQueryable<LoginRespuesta> Ingresar()
-//        {
-//            if (ValidarUsuario() && ValidarClave())
-//            {
-//                string token = TokenGenerator.GenerateTokenJwt(login.Usuario);
-//                return from U in dbExamen3.Set<AdministradorITM>()
-//                       join UP in dbSuper.Set<Usuario_Perfil>()
-//                       on U.id equals UP.idUsuario
-//                       join P in dbSuper.Set<Perfil>()
-//                       on UP.idPerfil equals P.id
-//                       where U.userName == login.Usuario &&
-//                               U.Clave == login.Clave
-//                       select new LoginRespuesta
-//                       {
-//                           Usuario = U.userName,
-//                           Autenticado = true,
-//                           Perfil = P.Nombre,
-//                           PaginaInicio = P.PaginaNavegar,
-//                           Token = token,
-//                           Mensaje = ""
-//                       };
-//            }
-//            else
-//            {
-//                List<LoginRespuesta> List = new List<LoginRespuesta>();
-//                List.Add(loginRespuesta);
-//                return List.AsQueryable();
-//            }
-//        }
-//    }
-//}
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Examen3.Models;
-using System.Web;
 
 namespace Examen3.Clases
 {
@@ -105,7 +12,7 @@ namespace Examen3.Clases
             loginRespuesta = new LoginRespuesta();
         }
 
-        public DBExamen3Entities dbExamen3 = new DBExamen3Entities();
+        public DBExamen3Entities db = new DBExamen3Entities();
         public Login login { get; set; }
         public LoginRespuesta loginRespuesta { get; set; }
 
@@ -113,28 +20,21 @@ namespace Examen3.Clases
         {
             try
             {
-                var administrador = dbExamen3.AdministradorITMs
-                    .FirstOrDefault(a => a.Usuario == login.Usuario);
-
-                if (administrador == null)
+                var admin = db.AdministradorITMs.FirstOrDefault(a => a.Usuario == login.Usuario);
+                if (admin == null)
                 {
                     loginRespuesta.Autenticado = false;
                     loginRespuesta.Mensaje = "Usuario no existe";
                     return false;
                 }
 
-                if (administrador.Clave != login.Clave)
+                if (admin.Clave != login.Clave)
                 {
                     loginRespuesta.Autenticado = false;
                     loginRespuesta.Mensaje = "Clave incorrecta";
                     return false;
                 }
 
-                loginRespuesta.Autenticado = true;
-                loginRespuesta.Usuario = administrador.Usuario;
-                loginRespuesta.Nombre = administrador.NombreCompleto;
-                loginRespuesta.Token = TokenGenerator.GenerateTokenJwt(administrador.Usuario);
-                loginRespuesta.Mensaje = "Autenticado correctamente";
                 return true;
             }
             catch (Exception ex)
@@ -145,10 +45,27 @@ namespace Examen3.Clases
             }
         }
 
-        public LoginRespuesta Ingresar()
+        public IQueryable<LoginRespuesta> Ingresar()
         {
-            ValidarUsuario();
-            return loginRespuesta;
+            if (ValidarUsuario())
+            {
+                string token = TokenGenerator.GenerateTokenJwt(login.Usuario);
+                return (from a in db.AdministradorITMs
+                        where a.Usuario == login.Usuario && a.Clave == login.Clave
+                        select new LoginRespuesta
+                        {
+                            Usuario = a.Usuario,
+                            Autenticado = true,
+                            Token = token,
+                            Mensaje = "Inicio de sesión exitoso"
+                        }).AsQueryable();
+            }
+            else
+            {
+                List<LoginRespuesta> list = new List<LoginRespuesta>();
+                list.Add(loginRespuesta);
+                return list.AsQueryable();
+            }
         }
     }
 }
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using Examen3.Clases;
+using Examen3.Models;
+using System.Linq;
+using System.Web.Http;
+
+namespace Examen3.Controllers
+{
+    [RoutePrefix("api/login")]
+    [AllowAnonymous]
+    public class LoginController : ApiController
+    {
+        [HttpPost]
+        [Route("Ingresar")]
+        public IQueryable<LoginRespuesta> Ingresar([FromBody] Login login)
+        {
+            clsLogin _login = new clsLogin();
+            _login.login = login;
+            return _login.Ingresar();
+        }
+    }
+}
