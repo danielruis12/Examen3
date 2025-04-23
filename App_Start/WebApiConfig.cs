b55c8e758657850d98068d45387172e3715ed2d9
Daniel Ruiz <daniru2770@gmail.com>
2025-04-23 15:27:22 -0500

    Se agrega en el archivo de WebApiConfig la habilitación de autenticación para la validación del token.

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Examen3.Clases;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -10,6 +11,8 @@ namespace Examen3
         public static void Register(HttpConfiguration config)
         {
             // Configuración y servicios de Web API
+            //Habilitar el esquema de autenticación, para la validación del token
+            config.MessageHandlers.Add(new TokenValidationHandler());
 
             // Rutas de Web API
             config.MapHttpAttributeRoutes();
